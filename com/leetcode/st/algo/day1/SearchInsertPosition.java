b5103d3514ea529ab4026723676ff251900187b7
vinay-sd17 <vinay.desikavinayagam@gmail.com>
2022-10-28 16:57:08 +0530

    Day 1 - solutions for ALgorithms

---
+++ com/leetcode/st/algo/day1/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+package com.leetcode.st.algo.day1;
+
+public class SearchInsertPosition {
+
+  public static void main(String[] args) {
+    int[] num = { 1, 3, 5, 6 };
+    System.out.println(searchInsert(num, 5));
+  }
+
+  //1,3,5,6
+
+  public static int searchInsert(int[] nums, int target) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return left;
+  }
+
+}
