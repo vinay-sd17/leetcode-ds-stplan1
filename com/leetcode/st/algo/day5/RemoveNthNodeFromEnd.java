ff23b10b36eaa2a53e7e8f505553568165b5107c
vinay-sd17 <vinay.desikavinayagam@gmail.com>
2022-10-28 16:57:08 +0530

    Day 5 algo solution

---
+++ com/leetcode/st/algo/day5/RemoveNthNodeFromEnd.java
@@ -0,0 +1,58 @@
+package com.leetcode.st.algo.day5;
+
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/?envType=study-plan&id=algorithm-i
+public class RemoveNthNodeFromEnd {
+
+  public static void main(String[] args) {
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode node4 = new ListNode(4);
+    ListNode node5 = new ListNode(5);
+
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = null;
+
+    ListNode listNode = removeNthFromEnd(node1, 2);
+  }
+
+  public static ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode dummy = new ListNode(0, head);
+    ListNode slow = dummy;
+    ListNode fast = dummy;
+    for (int i = 1; i <= n + 1; i++) {
+      fast = fast.next;
+    }
+
+    while (fast != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    slow.next = slow.next.next;
+    return dummy.next;
+  }
+
+}
+
+class ListNode {
+
+  int val;
+
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
